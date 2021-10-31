fecc3b9effdbbd653a5edea6dce92f3caa55531e
Victor <victorperezvillanueva123@gmail.com>
2021-10-31 15:45:20 +0100

    Modificar Resgistro

--- ProyectoTransacciones/src/main/java/Vista/Paneles/PanelRegistro.java
+++ ProyectoTransacciones/src/main/java/Vista/Paneles/PanelRegistro.java
@@ -7,14 +7,16 @@ package Vista.Paneles;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Insets;
-import java.awt.event.FocusAdapter;
-import java.awt.event.FocusEvent;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.MatteBorder;
 
@@ -24,10 +26,14 @@ import javax.swing.border.MatteBorder;
  */
 public class PanelRegistro extends PanelBackground {
 
+    private JPanel panelNacimiento;
     private JLabel titulo;
-    private JLabel txtDNI, txtNombre, txtApellido, txtPassword;
+    private JLabel txtDNI, txtNombre, txtApellido, txtPassword, txtNacimiento;
     private JTextField campoDNI, campoNombre, campoApellido, campoPassword;
+    private JComboBox listaDias, listaMeses, listaAños;
     private JButton botonConfirmar;
+
+    private String[] dias = new String[32], meses = new String[13], años = new String[201];
 
     private String fuentePrincipal = "Monospaced", fuenteSecundaria = "Arial";
     private Color colorPrincipal = new Color(218, 254, 235), colorSecundario = new Color(76, 138, 105);
@@ -70,6 +76,8 @@ public class PanelRegistro extends PanelBackground {
         campoNombre.setFont(new Font(fuenteSecundaria, Font.PLAIN, 16));
         campoNombre.setOpaque(false);
         campoNombre.setBorder(new MatteBorder(0, 0, 2, 0, colorSecundario));
+        g.insets = new Insets(10, 43, 0, 43);
+        g.fill = GridBagConstraints.HORIZONTAL;
         g.gridx = 0;
         g.gridy = 2;
         g.gridwidth = 2;
@@ -93,36 +101,86 @@ public class PanelRegistro extends PanelBackground {
         campoApellido.setFont(new Font(fuenteSecundaria, Font.PLAIN, 16));
         campoApellido.setOpaque(false);
         campoApellido.setBorder(new MatteBorder(0, 0, 2, 0, colorSecundario));
+        g.insets = new Insets(10, 43, 0, 43);
+        g.fill = GridBagConstraints.HORIZONTAL;
         g.gridx = 0;
         g.gridy = 4;
         g.gridwidth = 2;
         this.add(campoApellido, g);
         limpiarConstraints(g);
 
-        txtDNI = new JLabel("Introduce tu DNI:");
+        txtDNI = new JLabel("DNI:");
         txtDNI.setForeground(new Color(7, 65, 35));
         txtDNI.setFont(new Font(fuenteSecundaria, Font.BOLD, 18));
         g.insets = new Insets(20, 43, 0, 0);
         g.fill = GridBagConstraints.HORIZONTAL;
         g.gridx = 0;
         g.gridy = 5;
-        g.gridwidth = 2;
+        g.gridwidth = 1;
         this.add(txtDNI, g);
         limpiarConstraints(g);
 
-        //http://javaswingcomponents.blogspot.com/2012/05/how-to-create-simple-hinttextfield-in.html
         campoDNI = new JTextField();
         campoDNI.setPreferredSize(new Dimension(300, 35));
         campoDNI.setBorder(javax.swing.BorderFactory.createEmptyBorder());
         campoDNI.setFont(new Font(fuenteSecundaria, Font.PLAIN, 16));
         campoDNI.setOpaque(false);
         campoDNI.setBorder(new MatteBorder(0, 0, 2, 0, colorSecundario));
+        g.insets = new Insets(10, 43, 0, 0);
+        g.fill = GridBagConstraints.HORIZONTAL;
         g.gridx = 0;
         g.gridy = 6;
-        g.gridwidth = 2;
+        g.gridwidth = 1;
         this.add(campoDNI, g);
         limpiarConstraints(g);
-        
+
+        txtNacimiento = new JLabel("Fecha Nacimiento:");
+        txtNacimiento.setForeground(new Color(7, 65, 35));
+        txtNacimiento.setFont(new Font(fuenteSecundaria, Font.BOLD, 18));
+        g.insets = new Insets(20, 43, 0, 0);
+        g.fill = GridBagConstraints.HORIZONTAL;
+        g.gridx = 1;
+        g.gridy = 5;
+        g.gridwidth = 1;
+        this.add(txtNacimiento, g);
+        limpiarConstraints(g);
+
+        /**
+         * Panel donde iran los 3 comboBox del nacimiento
+         */
+        panelNacimiento = new JPanel(new FlowLayout());
+        g.insets = new Insets(10, 0, 0, 43);
+        g.fill = GridBagConstraints.HORIZONTAL;
+        g.gridx = 1;
+        g.gridy = 6;
+        g.gridwidth = 1;
+        this.add(panelNacimiento, g);
+        limpiarConstraints(g);
+
+        dias[0] = "- Dia -";
+        for (int i = 1; i < dias.length; i++) {
+            dias[i] = "" + (i);
+        }
+        listaDias = new JComboBox(dias);
+        listaDias.setPreferredSize(new Dimension(70, 25));
+        panelNacimiento.add(listaDias);
+
+        meses[0] = "- Mes -";
+        for (int i = 1; i < meses.length; i++) {
+            meses[i] = "" + (i);
+        }
+        listaMeses = new JComboBox(meses);
+        listaMeses.setPreferredSize(new Dimension(70, 25));
+        panelNacimiento.add(listaMeses);
+
+        años[0] = "- Año -";
+        for (int i = 1; i < años.length; i++) {
+            años[i] = "" + (i + 1900);
+        }
+        listaAños = new JComboBox(años);
+        listaAños.setPreferredSize(new Dimension(70, 25));
+        panelNacimiento.add(listaAños);
+
         txtPassword = new JLabel("Introduce tu Contraseña:");
         txtPassword.setForeground(new Color(7, 65, 35));
         txtPassword.setFont(new Font(fuenteSecundaria, Font.BOLD, 18));
@@ -140,6 +198,8 @@ public class PanelRegistro extends PanelBackground {
         campoPassword.setFont(new Font(fuenteSecundaria, Font.PLAIN, 16));
         campoPassword.setOpaque(false);
         campoPassword.setBorder(new MatteBorder(0, 0, 2, 0, colorSecundario));
+        g.fill = GridBagConstraints.HORIZONTAL;
+        g.insets = new Insets(10, 43, 0, 43);
         g.gridx = 0;
         g.gridy = 8;
         g.gridwidth = 2;
